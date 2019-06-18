68a69b9f6af77463c3ced203e214647355ff0420
ted <ted10401@gmail.com>
2019-06-18 22:21:21 +0800

    Polish the cases.

--- Assets/MarchingCubes.cs
+++ Assets/MarchingCubes.cs
@@ -8,6 +8,7 @@ public class MarchingCubes
     public float cubeSize = 1f;
 
     public Vector3[] nodePositions = new Vector3[8];
+    public Vector3[] edgePositions = new Vector3[12];
     public Vector3[] vertices = new Vector3[20];
     public List<int> triangles = new List<int>();
 
@@ -38,37 +39,37 @@ public class MarchingCubes
 
     private void UpdateVertices()
     {
-        vertices[0] = center + new Vector3(-1, -1, -1) * (cubeSize * 0.5f);
-        vertices[1] = center + new Vector3(0, -1, -1) * (cubeSize * 0.5f);
-        vertices[2] = center + new Vector3(1, -1, -1) * (cubeSize * 0.5f);
-        vertices[3] = center + new Vector3(1, -1, 0) * (cubeSize * 0.5f);
-        vertices[4] = center + new Vector3(1, -1, 1) * (cubeSize * 0.5f);
-        vertices[5] = center + new Vector3(0, -1, 1) * (cubeSize * 0.5f);
-        vertices[6] = center + new Vector3(-1, -1, 1) * (cubeSize * 0.5f);
-        vertices[7] = center + new Vector3(-1, -1, 0) * (cubeSize * 0.5f);
+        nodePositions[0] = center + new Vector3(-1, -1, -1) * (cubeSize * 0.5f);
+        nodePositions[1] = center + new Vector3(1, -1, -1) * (cubeSize * 0.5f);
+        nodePositions[2] = center + new Vector3(1, -1, 1) * (cubeSize * 0.5f);
+        nodePositions[3] = center + new Vector3(-1, -1, 1) * (cubeSize * 0.5f);
+        nodePositions[4] = center + new Vector3(-1, 1, -1) * (cubeSize * 0.5f);
+        nodePositions[5] = center + new Vector3(1, 1, -1) * (cubeSize * 0.5f);
+        nodePositions[6] = center + new Vector3(1, 1, 1) * (cubeSize * 0.5f);
+        nodePositions[7] = center + new Vector3(-1, 1, 1) * (cubeSize * 0.5f);
 
-        vertices[8] = center + new Vector3(-1, 0, -1) * (cubeSize * 0.5f);
-        vertices[9] = center + new Vector3(1, 0, -1) * (cubeSize * 0.5f);
-        vertices[10] = center + new Vector3(1, 0, 1) * (cubeSize * 0.5f);
-        vertices[11] = center + new Vector3(-1, 0, 1) * (cubeSize * 0.5f);
+        edgePositions[0] = center + new Vector3(0, -1, -1) * (cubeSize * 0.5f);
+        edgePositions[1] = center + new Vector3(1, -1, 0) * (cubeSize * 0.5f);
+        edgePositions[2] = center + new Vector3(0, -1, 1) * (cubeSize * 0.5f);
+        edgePositions[3] = center + new Vector3(-1, -1, 0) * (cubeSize * 0.5f);
+        edgePositions[4] = center + new Vector3(-1, 0, -1) * (cubeSize * 0.5f);
+        edgePositions[5] = center + new Vector3(1, 0, -1) * (cubeSize * 0.5f);
+        edgePositions[6] = center + new Vector3(1, 0, 1) * (cubeSize * 0.5f);
+        edgePositions[7] = center + new Vector3(-1, 0, 1) * (cubeSize * 0.5f);
+        edgePositions[8] = center + new Vector3(0, 1, -1) * (cubeSize * 0.5f);
+        edgePositions[9] = center + new Vector3(1, 1, 0) * (cubeSize * 0.5f);
+        edgePositions[10] = center + new Vector3(0, 1, 1) * (cubeSize * 0.5f);
+        edgePositions[11] = center + new Vector3(-1, 1, 0) * (cubeSize * 0.5f);
 
-        vertices[12] = center + new Vector3(-1, 1, -1) * (cubeSize * 0.5f);
-        vertices[13] = center + new Vector3(0, 1, -1) * (cubeSize * 0.5f);
-        vertices[14] = center + new Vector3(1, 1, -1) * (cubeSize * 0.5f);
-        vertices[15] = center + new Vector3(1, 1, 0) * (cubeSize * 0.5f);
-        vertices[16] = center + new Vector3(1, 1, 1) * (cubeSize * 0.5f);
-        vertices[17] = center + new Vector3(0, 1, 1) * (cubeSize * 0.5f);
-        vertices[18] = center + new Vector3(-1, 1, 1) * (cubeSize * 0.5f);
-        vertices[19] = center + new Vector3(-1, 1, 0) * (cubeSize * 0.5f);
+        for(int i = 0; i < nodePositions.Length; i++)
+        {
+            vertices[i] = nodePositions[i];
+        }
 
-        nodePositions[0] = vertices[0];
-        nodePositions[1] = vertices[2];
-        nodePositions[2] = vertices[4];
-        nodePositions[3] = vertices[6];
-        nodePositions[4] = vertices[12];
-        nodePositions[5] = vertices[14];
-        nodePositions[6] = vertices[16];
-        nodePositions[7] = vertices[18];
+        for (int i = 0; i < edgePositions.Length; i++)
+        {
+            vertices[i + nodePositions.Length] = edgePositions[i];
+        }
     }
 
     public void SetNodes(bool[] nodes)
@@ -96,133 +97,345 @@ public class MarchingCubes
 
         switch (configuration)
         {
-            case 0:
-            case 255:
+            case 0:   //00000000
+                break;
+            case 1:   //00000001
+                AddTriagnles(8, 11, 12);
+                break;
+            case 2:   //00000010
+                AddTriagnles(8, 13, 9);
+                break;
+            case 3:   //00000011
+                AddTriagnles(12, 13, 9, 12, 9, 11);
+                break;
+            case 4:   //00000100
+                AddTriagnles(9, 14, 10);
+                break;
+            case 5:   //00000101
+                AddTriagnles(8, 11, 12, 9, 14, 10);
+                break;
+            case 6:   //00000110
+                AddTriagnles(13, 14, 10, 13, 10, 8);
                 break;
 
-                //One Node
-            case 1:
-                AddTriagnles(1, 7, 8);
-                break;
-            case 2:
-                AddTriagnles(1, 9, 3);
-                break;
-            case 4:
-                AddTriagnles(3, 10, 5);
-                break;
-            case 8:
-                AddTriagnles(11, 7, 5);
-                break;
-            case 16:
-                AddTriagnles(8, 19, 13);
-                break;
-            case 32:
-                AddTriagnles(9, 13, 15);
-                break;
-            case 64:
-                AddTriagnles(10, 15, 17);
-                break;
-            case 128:
-                AddTriagnles(11, 17, 19);
+            case 7:   //00000111
                 break;
 
-                //Two Nodes
-                //1 + 2^N
-            case 3:
-                AddTriagnles(8, 9, 3, 8, 3, 7);
+            case 8:   //00001000
+                AddTriagnles(15, 11, 10);
                 break;
-            case 5:
-                AddTriagnles(1, 7, 8, 3, 10, 5);
+            case 9:   //00001001
+                AddTriagnles(15, 12, 8, 15, 8, 10);
                 break;
-            case 9:
-                AddTriagnles(11, 8, 1, 11, 1, 5);
-                break;
-            case 17:
-                AddTriagnles(13, 1, 7, 13, 7, 19);
-                break;
-            case 33:
-                AddTriagnles(1, 7, 8, 9, 13, 15);
-                break;
-            case 65:
-                AddTriagnles(1, 7, 8, 10, 15, 17);
-                break;
-            case 129:
-                AddTriagnles(1, 7, 8, 11, 17, 19);
+            case 10:  //00001010
+                AddTriagnles(8, 13, 9, 15, 11, 10);
                 break;
 
-                //2 + 2^N
-            case 6:
-                AddTriagnles(9, 10, 5, 9, 5, 1);
-                break;
-            case 10:
-                AddTriagnles(1, 9, 3, 11, 7, 5);
-                break;
-            case 18:
-                AddTriagnles(1, 9, 3, 8, 19, 13);
-                break;
-            case 34:
-                AddTriagnles(1, 13, 15, 1, 15, 3);
-                break;
-            case 66:
-                AddTriagnles(1, 9, 3, 10, 15, 17);
-                break;
-            case 130:
-                AddTriagnles(1, 9, 3, 11, 17, 19);
+            case 11:  //00001011
                 break;
 
-            //4 + 2^N
-            case 12:
-                AddTriagnles(10, 11, 7, 10, 7, 3);
-                break;
-            case 20:
-                AddTriagnles(3, 10, 5, 8, 19, 13);
-                break;
-            case 36:
-                AddTriagnles(3, 10, 5, 9, 13, 15);
-                break;
-            case 68:
-                AddTriagnles(3, 15, 17, 3, 17, 5);
-                break;
-            case 132:
-                AddTriagnles(3, 10, 5, 11, 17, 19);
+            case 12:  //00001100
+                AddTriagnles(14, 15, 11, 14, 11, 9);
                 break;
 
-                //8 + 2N
-            case 24:
-                AddTriagnles(11, 7, 5, 8, 19, 13);
+            case 13:   //00001101
                 break;
-            case 40:
-                AddTriagnles(11, 7, 5, 9, 13, 15);
+            case 14:   //00001110
                 break;
-            case 72:
-                AddTriagnles(11, 7, 5, 10, 15, 17);
-                break;
-            case 136:
-                AddTriagnles(19, 7, 5, 19, 5, 17);
+            case 15:   //00001111
                 break;
 
-                //16 + 2^N
-            case 48:
-                AddTriagnles(19, 15, 9, 19, 9, 8);
+            case 16:  //00010000
+                AddTriagnles(12, 19, 16);
                 break;
-            case 80:
-                AddTriagnles(8, 19, 13, 10, 15, 17);
+            case 17:  //00010001
+                AddTriagnles(16, 8, 11, 16, 11, 19);
                 break;
-            case 144:
-                AddTriagnles(17, 13, 8, 17, 8, 11);
-                break;
-
-                //32 + 2^N
-            case 96:
-                AddTriagnles(13, 17, 10, 13, 10, 9);
-                break;
-            case 160:
-                AddTriagnles(9, 13, 15, 11, 17, 19);
+            case 18:  //00010010
+                AddTriagnles(8, 13, 9, 12, 19, 16);
                 break;
 
-                //64 + 2^N
-            case 192:
-                AddTriagnles(15, 19, 11, 15, 11, 10);
+            case 19:  //00010011
+                break;
+
+            case 20:  //00010100
+                AddTriagnles(9, 14, 10, 12, 19, 16);
+                break;
+
+            case 21:  //00010101
+                break;
+            case 22:  //00010110
+                break;
+            case 23:  //00010111
+                break;
+
+            case 24:  //00011000
+                AddTriagnles(15, 11, 10, 12, 19, 16);
+                break;
+
+            case 25:  //00011001
+                break;
+            case 26:  //00011010
+                break;
+            case 27:  //00011011
+                break;
+            case 28:  //00011100
+                break;
+            case 29:  //00011101
+                break;
+            case 30:  //00011110
+                break;
+            case 31:  //00011111
+                break;
+
+            case 32:  //00100000
+                AddTriagnles(13, 16, 17);
+                break;
+            case 33:  //00100001
+                AddTriagnles(8, 11, 12, 13, 16, 17);
+                break;
+            case 34:  //00100010
+                AddTriagnles(8, 16, 17, 8, 17, 9);
+                break;
+
+            case 35:  //00100011
+                break;
+
+            case 36:  //00100100
+                AddTriagnles(9, 14, 10, 13, 16, 17);
+                break;
+
+            case 37:  //00100101
+                break;
+            case 38:  //00100110
+                break;
+            case 39:  //00100111
+                break;
+
+            case 40:  //00101000
+                AddTriagnles(15, 11, 10, 13, 16, 17);
+                break;
+
+            case 41:  //00101001
+                break;
+            case 42:  //00101010
+                break;
+            case 43:  //00101011
+                break;
+            case 44:  //00101100
+                break;
+            case 45:  //00101101
+                break;
+            case 46:  //00101110
+                break;
+            case 47:  //00101111
+                break;
+
+            case 48:  //00110000
+                AddTriagnles(19, 17, 13, 19, 13, 12);
+                break;
+
+            case 49:  //00110001
+                break;
+            case 50:  //00110010
+                break;
+            case 51:  //00110011
+                break;
+            case 52:  //00110100
+                break;
+            case 53:  //00110101
+                break;
+            case 54:  //00110110
+                break;
+            case 55:  //00110111
+                break;
+            case 56:  //00111000
+                break;
+            case 57:  //00111001
+                break;
+            case 58:  //00111010
+                break;
+            case 59:  //00111011
+                break;
+            case 60:  //00111100
+                break;
+            case 61:  //00111101
+                break;
+            case 62:  //00111110
+                break;
+            case 63:  //00111111
+                break;
+
+            case 64:  //01000000
+                AddTriagnles(14, 17, 18);
+                break;
+            case 65:  //01000001
+                AddTriagnles(8, 11, 12, 14, 17, 18);
+                break;
+            case 66:  //01000010
+                AddTriagnles(8, 13, 9, 14, 17, 18);
+                break;
+
+            case 67:  //01000011
+                break;
+
+            case 68:  //01000100
+                AddTriagnles(9, 17, 18, 9, 18, 10);
+                break;
+
+            case 69:  //01000101
+                break;
+            case 70:  //01000110
+                break;
+            case 71:  //01000111
+                break;
+
+            case 72:  //01001000
+                AddTriagnles(15, 11, 10, 14, 17, 18);
+                break;
+
+            case 73:  //01001001
+                break;
+            case 74:  //01001010
+                break;
+            case 75:  //01001011
+                break;
+            case 76:  //01001100
+                break;
+            case 77:  //01001101
+                break;
+            case 78:  //01001110
+                break;
+            case 79:  //01001111
+                break;
+
+            case 80:  //01010000
+                AddTriagnles(12, 19, 16, 14, 17, 18);
+                break;
+
+            case 81:  //01010001
+                break;
+            case 82:  //01010010
+                break;
+            case 83:  //01010011
+                break;
+            case 84:  //01010100
+                break;
+            case 85:  //01010101
+                break;
+            case 86:  //01010110
+                break;
+            case 87:  //01010111
+                break;
+            case 88:  //01011000
+                break;
+            case 89:  //01011001
+                break;
+            case 90:  //01011010
+                break;
+            case 91:  //01011011
+                break;
+            case 92:  //01011100
+                break;
+            case 93:  //01011101
+                break;
+            case 94:  //01011110
+                break;
+            case 95:  //01011111
+                break;
+
+            case 96:  //01100000
+                AddTriagnles(16, 18, 14, 16, 14, 13);
+                break;
+
+            case 97:  //01100001
+                break;
+            case 98:  //01100010
+                break;
+            case 99:  //01100011
+                break;
+            case 100: //01100100
+                break;
+            case 101: //01100101
+                break;
+            case 102: //01100110
+                break;
+            case 103: //01100111
+                break;
+            case 104: //01101000
+                break;
+            case 105: //01101001
+                break;
+            case 106: //01101010
+                break;
+            case 107: //01101011
+                break;
+            case 108: //01101100
+                break;
+            case 109: //01101101
+                break;
+            case 110: //01101110
+                break;
+            case 111: //01101111
+                break;
+            case 112: //01110000
+                break;
+            case 113: //01110001
+                break;
+            case 114: //01110010
+                break;
+            case 115: //01110011
+                break;
+            case 116: //01110100
+                break;
+            case 117: //01110101
+                break;
+            case 118: //01110110
+                break;
+            case 119: //01110111
+                break;
+            case 120: //01111000
+                break;
+            case 121: //01111001
+                break;
+            case 122: //01111010
+                break;
+            case 123: //01111011
+                break;
+            case 124: //01111100
+                break;
+            case 125: //01111101
+                break;
+            case 126: //01111110
+                break;
+            case 127: //01111111
+                break;
+
+            case 128: //10000000
+                AddTriagnles(15, 18, 19);
+                break;
+            case 129: //10000001
+                AddTriagnles(8, 11, 12, 15, 18, 19);
+                break;
+            case 130: //10000010
+                AddTriagnles(8, 13, 9, 15, 18, 19);
+                break;
+            case 132: //10000100
+                AddTriagnles(9, 14, 10, 15, 18, 19);
+                break;
+            case 136: //10001000
+                AddTriagnles(19, 11, 10, 19, 10, 18);
+                break;
+            case 144: //10010000
+                AddTriagnles(18, 16, 12, 18, 12, 15);
+                break;
+            case 160: //10100000
+                AddTriagnles(13, 16, 17, 15, 18, 19);
+                break;
+            case 192: //11000000
+                AddTriagnles(17, 19, 15, 17, 15, 14);
+                break;
+            case 255: //11111111
                 break;
         }
     }
--- Assets/UnitTesting_MarchingCubes.cs
+++ Assets/UnitTesting_MarchingCubes.cs
@@ -34,7 +34,20 @@ public class UnitTesting_MarchingCubes : MonoBehaviour
             return;
         }
 
-        for(int i = 0; i < m_marchingCubes.nodePositions.Length; i++)
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[0], m_marchingCubes.nodePositions[1]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[1], m_marchingCubes.nodePositions[2]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[2], m_marchingCubes.nodePositions[3]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[3], m_marchingCubes.nodePositions[0]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[4], m_marchingCubes.nodePositions[5]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[5], m_marchingCubes.nodePositions[6]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[6], m_marchingCubes.nodePositions[7]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[7], m_marchingCubes.nodePositions[4]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[0], m_marchingCubes.nodePositions[4]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[1], m_marchingCubes.nodePositions[5]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[2], m_marchingCubes.nodePositions[6]);
+        Gizmos.DrawLine(m_marchingCubes.nodePositions[3], m_marchingCubes.nodePositions[7]);
+
+        for (int i = 0; i < m_marchingCubes.nodePositions.Length; i++)
         {
             if(nodes[i])
             {
@@ -45,7 +58,31 @@ public class UnitTesting_MarchingCubes : MonoBehaviour
                 Gizmos.color = Color.white;
             }
 
+            if(m_marchingCubes.nodePositions[i].y > 0)
+            {
+                UnityEditor.Handles.Label(m_marchingCubes.nodePositions[i] + Vector3.up * 0.2f, i.ToString());
+            }
+            else
+            {
+                UnityEditor.Handles.Label(m_marchingCubes.nodePositions[i] + Vector3.up * -0.1f, i.ToString());
+            }
+
             Gizmos.DrawSphere(m_marchingCubes.nodePositions[i], 0.1f);
+        }
+
+        Gizmos.color = Color.grey;
+        for (int i = 0; i < m_marchingCubes.edgePositions.Length; i++)
+        {
+            if (m_marchingCubes.edgePositions[i].y > 0)
+            {
+                UnityEditor.Handles.Label(m_marchingCubes.edgePositions[i] + Vector3.up * 0.2f, (i + 8).ToString());
+            }
+            else
+            {
+                UnityEditor.Handles.Label(m_marchingCubes.edgePositions[i] + Vector3.up * -0.1f, (i + 8).ToString());
+            }
+
+            Gizmos.DrawSphere(m_marchingCubes.edgePositions[i], 0.05f);
         }
     }
 }
